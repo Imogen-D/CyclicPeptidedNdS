a8dc115f53d242abbdcdd5c7a9a07ecf38c005f9
Imogen-D <57735780+Imogen-D@users.noreply.github.com>
2020-02-18 14:08:12 +1300

    Create dNdSplotting.R

---
+++ dNdSplotting.R
@@ -0,0 +1,11 @@
+#code to plot dNdS values
+#dNdS made in ubuntu
+
+dNdS <- read.csv("~/CoxExtension/dNdS.csv", header=FALSE, stringsAsFactors=FALSE)
+dNdS <- dNdS[-1,]
+colnames(dNdS) <- c("ortholog", "dNdS_values")
+
+plot <- ggplot(dNdS, aes(dNdS_values, ortholog)) + geom_point(aes(), colour="black", pch=21)
+plot
+
+#plot produced of ortholog numbers and dNdSvalues
