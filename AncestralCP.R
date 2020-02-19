92e2c396937b58b955363caa65bf31aa5b68d280
Imogen-D <57735780+Imogen-D@users.noreply.github.com>
2020-02-20 09:21:33 +1300

    Update AncestralCP.R

--- AncestralCP.R
+++ AncestralCP.R
@@ -17,34 +17,12 @@ library("rentrez")
 library(RColorBrewer)
 library(magick)
 
-ortho_long <- read.delim("~/CoxExtension/ortho_long.tsv", header=FALSE, stringsAsFactors=FALSE)
-CPTrueFalse <- read.delim("~/CoxExtension/CPTrueFalse.csv", header=FALSE, stringsAsFactors=FALSE)
-CPorthos <- unique(CPTrueFalse$V1)
-
-ortho_long_rows <- lapply(CPorthos, function(x) which(ortho_long$V1 == (x)))
-rownums <- unlist(ortho_long_rows)
-subsetorthos <- ortho_long[(rownums),]
-
-write.table(subsetorthos, file = "subsetCP.tsv", row.names = FALSE, col.names = FALSE, quote = FALSE)
-
-
-#dNdS made in ubuntu
-dNdS <- read.csv("~/CoxExtension/dNdS.csv", header=FALSE, stringsAsFactors=FALSE)
-dNdS <- dNdS[-1,]
-colnames(dNdS) <- c("ortholog", "dNdS_values")
-
-plot <- ggplot(dNdS, aes(dNdS_values, ortholog)) + geom_point(aes(), colour="black", pch=21)
-plot
-#plot produced of ortholog and 
-
-
-
-
-branchtree <- read.tree(file = "~/Shared Folder/RAxML_result.fullbranchlengthstree")
+                          
+branchtree <- read.tree(file = "~/Shared Folder/RAxML_result.fullbranchlengthstree") #produced by concatenate.py
 rooted_branchtree <- root(branchtree, "CCE27021")
 droppedtip <- drop.tip(rooted_branchtree, "CCE27021")
 plot(droppedtip)
-axisPhylo()
+axisPhylo() #observe tree
 
 
 pa <- read.delim("./CoxExtension/pa.csv", header=FALSE, stringsAsFactors=FALSE)
@@ -61,7 +39,8 @@ chronos_relative <- makeChronosCalib(droppedtip, node = "root", age.min = 1, age
 chronos_relative #pretending to add chronos / how to add to tree?
 
 Q <- matrix( c(0,1,2,0) , nrow=2)
-ancestral <- ace(matrixog, droppedtip, type = "discrete", model = Q)  #currently ASTRAL no branch lengths so doing concantenation first
+ancestral <- ace(matrixog, droppedtip, type = "discrete", model = Q)
+#had to produce matrix for all rates different model manually as ARD had errors
 margancesstates <- round(ancestral$lik.anc,2)
 
 nodelabels(node=1:droppedtip$Nnode+Ntip(droppedtip), pie=ancestral$lik.anc,cex=0.5)
