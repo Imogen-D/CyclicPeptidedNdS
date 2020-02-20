e49f5b47a2036d3ad6a7c21e2b81891fcf3e1eb2
Imogen-D <57735780+Imogen-D@users.noreply.github.com>
2020-02-21 10:17:19 +1300

    Update OrthologSubsetting.R

--- OrthologSubsetting.R
+++ OrthologSubsetting.R
@@ -19,22 +19,3 @@ write.table(CP_orthos, file = "subsetCP.tsv", row.names = FALSE, col.names = FAL
 
 
 #this tsv then used to produce dNdS values
-
-#########################################
-# Probably move this to another script  #
-#########################################
-
-#Subsetting all orthologs for concatenate.py / making branch lengths
-                          
-files <- which(Ntip(<all trees read into R>) == 24) #total number of strains
-filepaths <- all_filepath_trees[files]
-og_nums_full <- str_extract(string = filepaths, pattern = "og_\\d+")
-
-str_subset(ortho_long$V1, CPorthos[1])
-ortho_full_rows <- lapply(og_nums_full, function(x) which(ortho_long$V1 == (x)))
-rownums <- unlist(ortho_full_rows)
-subsetorthos <- ortho_long[(rownums),]
-
-full <- unique(subsetorthos$V1)
-write.table(full, file = "subsetfull.tsv", row.names = FALSE, col.names = FALSE, quote = FALSE)
-
