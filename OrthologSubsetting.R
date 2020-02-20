a3fc5bb523657a67dbe43595dfe79168abbf2918
Imogen-D <57735780+Imogen-D@users.noreply.github.com>
2020-02-21 09:10:53 +1300

    Update OrthologSubsetting.R

--- OrthologSubsetting.R
+++ OrthologSubsetting.R
@@ -3,7 +3,7 @@
 #are subsetted from full ortholog id file
 
 ortho_long <- read.delim("<ortho_long.tsv>", header=FALSE, stringsAsFactors=FALSE)
-pa <- read.delim("<presence of ortholog files.csv", header=FALSE, stringsAsFactors=FALSE) #pa.csv
+pa <- read.delim("<presence of ortholog files.csv>", header=FALSE, stringsAsFactors=FALSE) #pa.csv
 CPorthos <- unique(pa$V1)
 
 ortho_long_rows <- lapply(CPorthos, function(x) which(ortho_long$V1 == (x)))
@@ -14,3 +14,19 @@ write.table(subsetorthos, file = "subsetCP.tsv", row.names = FALSE, col.names = 
 
 
 #this tsv then used to produce dNdS values
+
+
+#Subsetting all orthologs for concatenate.py / making branch lengths
+                          
+files <- which(Ntip(<all trees read into R>) == 24) #total number of strains
+filepaths <- all_filepath_trees[files]
+og_nums_full <- str_extract(string = filepaths, pattern = "og_\\d+")
+
+str_subset(ortho_long$V1, CPorthos[1])
+ortho_full_rows <- lapply(og_nums_full, function(x) which(ortho_long$V1 == (x)))
+rownums <- unlist(ortho_full_rows)
+subsetorthos <- ortho_long[(rownums),]
+
+full <- unique(subsetorthos$V1)
+write.table(full, file = "subsetfull.tsv", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
